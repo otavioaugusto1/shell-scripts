a3327dbaf849f26cf0876362d3984ea87ce5be07
Otávio Augusto Gonçalves Nogueira <56416850+otavioaugusto1@users.noreply.github.com>
2023-02-08 11:54:21 -0300

    Create variaveis-reservadas.sh

---
+++ variaveis-reservadas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Primeiro parâmetro: $1"
+
+echo "Segundo parâmetro: $2"
+
+echo "Todos os parâmetros: $*"
+
+echo "Quantidade de parâmetros: $#"
+
+echo "Processo atual: $$"
+
+echo "Nome do arquivo: $0"
+
+
+
+
