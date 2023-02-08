ae076f4964b108842b03ffe8b18bce815bd99533
Otávio Augusto Gonçalves Nogueira <56416850+otavioaugusto1@users.noreply.github.com>
2023-02-08 11:55:44 -0300

    Update while.sh

--- while.sh
+++ while.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+frutas=(
+	'abacaxi'
+	'Laranja'
+	'Acerola'
+)
+
+CONTADOR=0
+while [ $CONTADOR -lt ${#frutas[@]} ]; 
+do
+    echo "$CONTADOR: ${frutas[$CONTADOR]}"
+
+    CONTADOR=$(($CONTADOR + 1))
+done
+
+#lendo através de um arquivo externo
+#while read linha;
+#do
+#	echo "linha - $linha"
+#done < /etc/passwd
+
 while true; do
 echo "sleeping..."
 sleep 4
 done
+
+
